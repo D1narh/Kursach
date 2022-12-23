87a0b7c9d4342331d5a44225fccf3c9c258c90b2
D1narh <dunis.guryanov@mail.ru>
2022-12-24 01:02:11 +0400

    Добавьте файлы проекта.

---
+++ MaimApp/Views/MainProduct.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MaimApp.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainProduct.xaml
+    /// </summary>
+    public partial class MainProduct : Window
+    {
+        public MainProduct()
+        {
+            InitializeComponent();
+
+            AirTickets.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+            BusTickets.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+            Hotels.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+            Adventures.Background = new SolidColorBrush(Colors.Black) { Opacity = 0.3 };
+        }
+
+        private void AirTickets_MouseEnter(object sender, MouseEventArgs e)
+        {
+            AnimationEnter(AirTickets);
+        }
+
+        private void AirTickets_MouseLeave(object sender, MouseEventArgs e)
+        {
+            AnimationLeave(AirTickets);
+        }
+
+        private void BusTickets_MouseEnter(object sender, MouseEventArgs e)
+        {
+            AnimationEnter(BusTickets);
+        }
+
+        private void BusTickets_MouseLeave(object sender, MouseEventArgs e)
+        {
+            AnimationLeave(BusTickets);
+        }
+
+        private void Adventures_MouseEnter(object sender, MouseEventArgs e)
+        {
+            AnimationEnter(Adventures);
+        }
+
+        private void Adventures_MouseLeave(object sender, MouseEventArgs e)
+        {
+            AnimationLeave(Adventures);
+        }
+
+        private void Hotels_MouseEnter(object sender, MouseEventArgs e)
+        {
+            AnimationEnter(Hotels);
+        }
+
+        private void Hotels_MouseLeave(object sender, MouseEventArgs e)
+        {
+            AnimationLeave(Hotels);
+        }
+        public void AnimationEnter(Button name)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            anim.To = 140;
+            anim.Duration = TimeSpan.FromSeconds(0.1);
+            name.BeginAnimation(WidthProperty, anim);
+        }
+        public void AnimationLeave(Button name)
+        {
+            DoubleAnimation anim = new DoubleAnimation();
+            anim.To = 130;
+            anim.Duration = TimeSpan.FromSeconds(0.1);
+            name.BeginAnimation(WidthProperty, anim);
+        }
+    }
+}
